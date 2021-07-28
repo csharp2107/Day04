56ec6670c42c91aabe413986ab0c62ce8cc60be7
kurs <kurs@DESKTOP-V3HDKPQ>
2021-07-28 12:02:16 +0200

    tcp server

--- SerializationExample/PerformanceCheck.cs
+++ SerializationExample/PerformanceCheck.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using System.Diagnostics;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 
 namespace SerializationExample
 {
@@ -35,6 +37,7 @@ namespace SerializationExample
             // serialization
             string DEST_FILE = "c:/tmp/performance.xml";
 
+            // XML Serializer 
             Stopwatch sw = new Stopwatch();
             sw.Start();
             using (FileStream fs = new FileStream(DEST_FILE, FileMode.Create))
@@ -54,6 +57,38 @@ namespace SerializationExample
             }
             sw.Stop();
             Console.WriteLine($"Deserialization XML: {sw.ElapsedMilliseconds}");
+
+
+            // JSON 1
+            DEST_FILE = "c:/tmp/performance1.json";
+            sw.Restart();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            js.MaxJsonLength = 60_000_000;
+            String s = js.Serialize(empList);
+            File.WriteAllText(DEST_FILE, s);
+            sw.Stop();
+            Console.WriteLine($"Serialization JSON1: {sw.ElapsedMilliseconds}");
+
+            sw.Restart();
+            s = File.ReadAllText(DEST_FILE);
+            List<Employee> empList1 = js.Deserialize<List<Employee>>(s);
+            sw.Stop();
+            Console.WriteLine($"Deserialization JSON1: {sw.ElapsedMilliseconds}");
+
+
+            // JSON2 - JSON.NET library
+            DEST_FILE = "c:/tmp/performance2.json";
+            sw.Restart();
+            s = JsonConvert.SerializeObject(empList);
+            File.WriteAllText(DEST_FILE, s);
+            sw.Stop();
+            Console.WriteLine($"Serialization JSON2: {sw.ElapsedMilliseconds}");
+
+            sw.Restart();
+            s = File.ReadAllText(DEST_FILE);
+            empList1 = JsonConvert.DeserializeObject<List<Employee>>(s);
+            sw.Stop();
+            Console.WriteLine($"Deserialization JSON2: {sw.ElapsedMilliseconds}");
 
             Console.ReadKey();
 
---
+++ ServerClientExample/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerClientExample
+{
+    class Server
+    {
+        TcpListener server = null;
+
+        public Server(string ip, int port)
+        {
+            IPAddress localAddr = IPAddress.Parse(ip);
+            server = new TcpListener(localAddr, port);
+            server.Start();
+            StartListener();
+        }
+
+        private void StartListener()
+        {
+            Console.WriteLine("Waiting for connections..");
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("connected");
+                    Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                    t.Start(client);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+                server.Stop();
+            }
+        }
+
+        public void HandleClient(Object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            var stream =  client.GetStream();
+        }
+    }
+}
